a990c1f3bcdf0fc689d56745766a08077666ad26
silviamartina1 <47982128+silviamartina1@users.noreply.github.com>
2023-07-26 16:37:25 +0200

    Update Budget.js
    prova_Budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -9,3 +9,38 @@ const Budget = () => {
     );
 };
 export default Budget;
+
+
+import React, { useContext, useState  } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [Budget, setbudget] = useState('');
+    const submitEventBudget=() => {
+
+        if (Budget > 20000) {
+            alert("The value cannot exceed 20000");
+            setbudget("");
+            return;
+        }
+        return (
+            <div className='alert alert-secondary'>
+                <span>Budget: £{budget}</span>
+                <input
+                    required='required'
+                    type='number'
+                    id='Budget'
+                    value={Budget}
+                    style={{ marginLeft: '2rem', size: 10 }}
+                    onChange={(event) => setbudget(event.target.value)}>
+                </input>
+
+                <button className="btn btn-primary" onClick={submitEventBudget} style={{ marginLeft: '2rem' }}>
+                 Save
+                </button>
+            </div>
+
+        );
+    }
+};
+export default Budget;
